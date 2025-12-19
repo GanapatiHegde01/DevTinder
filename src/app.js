8b7eaaf02ff4e84a64bbd25471c1decac7fbae5a
GanapatiHegde01 <ganapatihegde027@gmail.com>
2025-12-19 16:22:35 +0530

    routing basics

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello wordl");
+app.get("/user/:id/:name", (req, res) => {
+  const params = {...req.params}
+   console.log(params);
+  res.send("Welcome user");
 });
+
+app.post("/us{e}r", (req, res) => {
+ 
+  
+  res.send({
+    firstname: "Ganapati",
+    lastname: "Hegde",
+  });
+});
+  app.put(/\.com$/, (req, res) => {
+    res.send("Put method")
+  });
+
+  app.patch("/user", (req, res) => {
+    res.send("patch method");
+  });
+
+  app.delete("/user", (req, res) => {
+    res.send("delete method");
+  });
 
 app.listen(7777, () => {
   console.log("Server Started Listening to port 7777");
